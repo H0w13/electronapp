32b1156ca53995aba3133d791febdc6b0eaa7a72
Howie Ching <howieching@gmail.com>
2016-06-10 19:51:59 +0800

    remove generated file and fix the requirejs issue

--- Folderion/app/scripts/controllers/OperationController.js
+++ Folderion/app/scripts/controllers/OperationController.js
@@ -1,11 +1,11 @@
-define(["folderion"], function(folderion) {
+define(["folderion"], function (folderion) {
 
-	folderion.controller("operation", function($scope, fileservice) {
+	folderion.controller("operation", function ($scope, fileservice) {
 
-		var getNextLevel = function(path, node, level) {
+		var getNextLevel = function (path, node, level) {
 			if (level > 3)
 				return;
-			fileservice.getSubItems(path, function(err, files) {
+			fileservice.getSubItems(path, function (err, files) {
 				if (err)
 					console.log(err);
 				else {
@@ -21,14 +21,14 @@ define(["folderion"], function(folderion) {
 			});
 		};
 
-		var drawMap = function(root) {
+		var drawMap = function (root) {
 			$("#map").empty();
 			var margin = {
-					top: 40,
-					right: 10,
-					bottom: 10,
-					left: 10
-				},
+				top: 40,
+				right: 10,
+				bottom: 10,
+				left: 10
+			},
 				width = 960 - margin.left - margin.right,
 				height = 500 - margin.top - margin.bottom;
 
@@ -37,7 +37,7 @@ define(["folderion"], function(folderion) {
 			var treemap = d3.layout.treemap()
 				.size([width, height])
 				.sticky(true)
-				.value(function(d) {
+				.value(function (d) {
 					return d.size;
 				});
 
@@ -53,32 +53,32 @@ define(["folderion"], function(folderion) {
 				.enter().append("div")
 				.attr("class", "node")
 				.call(position)
-				.style("background", function(d) {
+				.style("background", function (d) {
 					return d.children ? color(d.name) : null;
 				})
-				.text(function(d) {
+				.text(function (d) {
 					return d.children ? null : d.name;
 				});
 
 			function position() {
-				this.style("left", function(d) {
-						return d.x + "px";
-					})
-					.style("top", function(d) {
+				this.style("left", function (d) {
+					return d.x + "px";
+				})
+					.style("top", function (d) {
 						return d.y + "px";
 					})
-					.style("width", function(d) {
+					.style("width", function (d) {
 						return Math.max(0, d.dx - 1) + "px";
 					})
-					.style("height", function(d) {
+					.style("height", function (d) {
 						return Math.max(0, d.dy - 1) + "px";
 					});
 			}
 		};
 
 		$scope.folderPath = "";
-		$scope.loadFolder = function() {
-			fileservice.getSubItems($scope.folderPath, function(err, files) {
+		$scope.loadFolder = function () {
+			fileservice.getSubItems($scope.folderPath, function (err, files) {
 				if (err)
 					throw err;
 				else {
@@ -89,13 +89,18 @@ define(["folderion"], function(folderion) {
 						}
 						nodes.push(files[i]);
 					}
-					requirejs(["ztree"]);
-					$.fn.zTree.init($("#tree"), {}, nodes);
-					var root = {
-						name: "flare",
-						children: nodes
-					};
-					drawMap(root);
+					requirejs(["jquery"], function ($) {
+						requirejs(["ztree"], function (ztree) {
+							$.fn.zTree.init($("#tree"), {}, nodes);
+						});
+						requirejs(["d3"], function (d3) {
+							var root = {
+								name: "flare",
+								children: nodes
+							};
+							drawMap(root);
+						});
+					});
 				}
 			});
 		};
