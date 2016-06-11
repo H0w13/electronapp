113cad1e0212b929b2bc8b8e1ac7c137a02c1210
Howie Ching <howieching@gmail.com>
2016-06-12 03:03:43 +0800

    add navbar to switch between treemap and file list

--- Folderion/app/scripts/controllers/boardcontroller.js
+++ Folderion/app/scripts/controllers/boardcontroller.js
@@ -3,27 +3,40 @@ define(["folderion"], function (folderion) {
         $scope.boardType = "filelist";
         $scope.$on('RootDrawTreeMap', function (event, files) {
             $scope.files = files;
+            $scope.boardType = "filelist";
             var root = {
                 name: "flare",
                 children: files
             };
             requirejs(["jquery"], function ($) {
                 $("#board").empty();
-                if ($scope.boardType == "filelist") {
-                    $http.get("templates/filelist.html").then(function (result) {;
-                        var scope= angular.element(document.getElementById("board")).scope();
-                        var compile=$compile($(result.data))(scope.$new());                        
-                        $("#board").append(compile);
-                        //scope.$apply();
-                    });
-                }
-                else {
-                    chartservice.drawTreeMap(root, "board");
-                }
-
-
+                $http.get("templates/filelist.html").then(function (result) {
+                    var scope = angular.element(document.getElementById("board")).scope();
+                    var compile = $compile($(result.data))(scope.$new());
+                    $("#board").append(compile);
+                });
             });
         });
 
+        $scope.drawBoard = function (type) {
+            $scope.boardType = type;
+            requirejs(["jquery"], function ($) {
+                $("#board").empty();
+                if ($scope.boardType == 'filelist') {
+                    $http.get("templates/filelist.html").then(function (result) {
+                        var scope = angular.element(document.getElementById("board")).scope();
+                        var compile = $compile($(result.data))(scope.$new());
+                        $("#board").append(compile);
+                    });
+                }
+                else if ($scope.boardType == 'treemap') {
+                    var root = {
+                        name: "flare",
+                        children: $scope.files
+                    };
+                    chartservice.drawTreeMap(root, "board");
+                }
+            });
+        };
     });
 });
