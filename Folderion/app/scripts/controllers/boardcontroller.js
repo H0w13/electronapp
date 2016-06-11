063763e949128282e498820e99debde88e39c09f
Howie Ching <howieching@gmail.com>
2016-06-12 02:21:50 +0800

    add template for file list

--- Folderion/app/scripts/controllers/boardcontroller.js
+++ Folderion/app/scripts/controllers/boardcontroller.js
@@ -1,5 +1,6 @@
 define(["folderion"], function (folderion) {
-    folderion.controller("board", function ($scope, chartservice) {
+    folderion.controller("board", function ($scope, $http, chartservice, $compile) {
+        $scope.boardType = "filelist";
         $scope.$on('RootDrawTreeMap', function (event, files) {
             $scope.files = files;
             var root = {
@@ -8,7 +9,19 @@ define(["folderion"], function (folderion) {
             };
             requirejs(["jquery"], function ($) {
                 $("#board").empty();
-                chartservice.drawTreeMap(root, "board");
+                if ($scope.boardType == "filelist") {
+                    $http.get("templates/filelist.html").then(function (result) {;
+                        var scope= angular.element(document.getElementById("board")).scope();
+                        var compile=$compile($(result.data))(scope.$new());                        
+                        $("#board").append(compile);
+                        //scope.$apply();
+                    });
+                }
+                else {
+                    chartservice.drawTreeMap(root, "board");
+                }
+
+
             });
         });
 
