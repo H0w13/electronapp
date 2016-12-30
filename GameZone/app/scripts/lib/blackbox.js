938b51b6e69fe67211c183b75bd2172e03dfc7f0
Howie Ching <howieching@gmail.com>
2016-12-30 20:59:42 +0800

    draw marker and laser path

--- GameZone/app/scripts/lib/blackbox.js
+++ GameZone/app/scripts/lib/blackbox.js
@@ -1,22 +1,227 @@
 ;
 const blackboxCellModel = require("../models/blackboxmodel.js");
-module.exports = (function(){
+const commonHelper = require("../lib/common.js");
+module.exports = (function () {
     var blackbox = {
         board: []
     };
-    blackbox.init = function(){
-        for(var i = 0; i < 10; i++)
-        {
+    blackbox.init = function () {
+        for (var i = 0; i < 10; i++) {
             var row = [];
-            for(var j = 0; j < 10; j++)
-            {
+            for (var j = 0; j < 10; j++) {
                 row.push(blackboxCellModel(i, j));
             }
             blackbox.board.push(row);
         }
-    };
-    blackbox.showLines = function(){};
+        //generate puzzle
+        var moveDirection = {
+            LEFT: 0,
+            RIGHT: 1,
+            TOP: 2,
+            BOTTOM: 3
+        };
+        var LineType = {
+            BLANK: 0,
+            BOTTOM_LEFT: 6,
+            BOTTOM_LEFT_RIGHT: 14,
+            BOTTOM_RIGHT: 10,
+            CROSS: 15,
+            HORIZONTAL: 12,
+            TOP_BOTTOM_LEFT: 7,
+            TOP_BOTTOM_RIGHT: 11,
+            TOP_LEFT: 5,
+            TOP_LEFT_RIGHT: 13,
+            TOP_RIGHT: 9,
+            VERTICAL: 3,
+        };
+        var moveNext = function (cell, direction) {
+            var x = cell.indexX;
+            var y = cell.indexY;
+            console.log(x + ", " + y);
+            var nextCell = null;
+            var newDirection = direction;
+            if (direction == moveDirection.RIGHT) {
+                if(blackbox.board[x][y + 1].isSysMarked)
+                {
+                    blackbox.board[x][y].lineType |= LineType.HORIZONTAL;
+                    nextCell = blackbox.board[x][y + 1];
+                }
+                else if (blackbox.board[x - 1][y + 1].isSysMarked) {
+                    blackbox.board[x][y].lineType |= LineType.BOTTOM_LEFT;
+                    nextCell = blackbox.board[x + 1][y];
+                    newDirection = moveDirection.BOTTOM;
+                }
+                else if (blackbox.board[x + 1][y + 1].isSysMarked) {
+                    blackbox.board[x][y].lineType |= LineType.TOP_LEFT;
+                    nextCell = blackbox.board[x - 1][y];
+                    newDirection = moveDirection.TOP;
+                }
+                else {
+                    blackbox.board[x][y].lineType |= LineType.HORIZONTAL;
+                    nextCell = blackbox.board[x][y + 1];
+                }
+            }
+            else if (direction == moveDirection.LEFT) {
+                if(blackbox.board[x][y - 1].isSysMarked)
+                {
+                    blackbox.board[x][y].lineType |= LineType.HORIZONTAL;
+                    nextCell = blackbox.board[x][y - 1];
+                }
+                else if (blackbox.board[x - 1][y - 1].isSysMarked) {
+                    blackbox.board[x][y].lineType |= LineType.BOTTOM_RIGHT;
+                    nextCell = blackbox.board[x + 1][y];
+                    newDirection = moveDirection.BOTTOM;
+                }
+                else if (blackbox.board[x + 1][y - 1].isSysMarked) {
+                    blackbox.board[x][y].lineType |= LineType.TOP_RIGHT;
+                    nextCell = blackbox.board[x - 1][y];
+                    newDirection = moveDirection.TOP;
+                }
+                else {
+                    blackbox.board[x][y].lineType |= LineType.HORIZONTAL;
+                    nextCell = blackbox.board[x][y - 1];
+                }
+            }
+            else if (direction == moveDirection.TOP) {
+                if(blackbox.board[x - 1][y].isSysMarked)
+                {
+                    blackbox.board[x][y].lineType |= LineType.VERTICAL;
+                    nextCell = blackbox.board[x - 1][y];
+                }
+                else if (blackbox.board[x - 1][y - 1].isSysMarked) {
+                    blackbox.board[x][y].lineType |= LineType.BOTTOM_RIGHT;
+                    nextCell = blackbox.board[x][y + 1];
+                    newDirection = moveDirection.RIGHT;
+                }
+                else if (blackbox.board[x - 1][y + 1].isSysMarked) {
+                    blackbox.board[x][y].lineType |= LineType.BOTTOM_LEFT;
+                    nextCell = blackbox.board[x][y - 1];
+                    newDirection = moveDirection.LEFT;
+                }
+                else {
+                    blackbox.board[x][y].lineType |= LineType.VERTICAL;
+                    nextCell = blackbox.board[x - 1][y];
+                }
+            }
+            else if (direction == moveDirection.BOTTOM) {
+                if(blackbox.board[x + 1][y].isSysMarked)
+                {
+                    blackbox.board[x][y].lineType |= LineType.VERTICAL;
+                    nextCell = blackbox.board[x + 1][y];
+                }
+                else if (blackbox.board[x + 1][y - 1].isSysMarked) {
+                    blackbox.board[x][y].lineType |= LineType.TOP_RIGHT;
+                    nextCell = blackbox.board[x][y + 1];
+                    newDirection = moveDirection.RIGHT;
+                }
+                else if (blackbox.board[x + 1][y + 1].isSysMarked) {
+                    blackbox.board[x][y].lineType |= LineType.TOP_LEFT;
+                    nextCell = blackbox.board[x][y - 1];
+                    newDirection = moveDirection.LEFT;
+                }
+                else {
+                    blackbox.board[x][y].lineType |= LineType.VERTICAL;
+                    nextCell = blackbox.board[x + 1][y];
+                }
+            }
+            blackbox.board[x][y].updateStyle();
+            return [nextCell, newDirection];
+        };
 
+        for (var i = 0; i < 5; i++) {
+            var x = commonHelper.getRandomIntInclusive(1, 8);
+            var y = commonHelper.getRandomIntInclusive(1, 8);
+            while (blackbox.board[x][y].isSysMarked) {
+                x = commonHelper.getRandomIntInclusive(1, 8);
+                y = commonHelper.getRandomIntInclusive(1, 8);
+            }
+            blackbox.board[x][y].isSysMarked = true;
+            blackbox.board[x][y].updateStyle();
+        }
+        for (var i = 1; i < 9; i++) {
+            var isEnd = false;
+            var xIndex = i;
+            var yIndex = 1;
+            var direction = moveDirection.RIGHT;
+            while (!isEnd) {
+                var value = moveNext(blackbox.board[xIndex][yIndex], direction);
+                if (value[0] == null)
+                    isEnd = true;
+                else if (value[0].indexX == 0 || value[0].indexX == 9 || value[0].indexY == 0 || value[0].indexY == 9)
+                    isEnd = true;
+                else if (value[0].isSysMarked)
+                    isEnd = true;
+                else {
+                    direction = value[1]
+                    xIndex = value[0].indexX;
+                    yIndex = value[0].indexY;
+                }
+            }
+        } 
+        for (var i = 1; i < 9; i++) {
+            var isEnd = false;
+            var xIndex = 1;
+            var yIndex = i;
+            var direction = moveDirection.BOTTOM;
+            while (!isEnd) {
+                var value = moveNext(blackbox.board[xIndex][yIndex], direction);
+                if (value[0] == null)
+                    isEnd = true;
+                else if (value[0].indexX == 0 || value[0].indexX == 9 || value[0].indexY == 0 || value[0].indexY == 9)
+                    isEnd = true;
+                else if (value[0].isSysMarked)
+                    isEnd = true;
+                else {
+                    direction = value[1]
+                    xIndex = value[0].indexX;
+                    yIndex = value[0].indexY;
+                }
+            }
+        } 
+        for (var i = 1; i < 9; i++) {
+            var isEnd = false;
+            var xIndex = i;
+            var yIndex = 8;
+            var direction = moveDirection.LEFT;
+            while (!isEnd) {
+                var value = moveNext(blackbox.board[xIndex][yIndex], direction);
+                if (value[0] == null)
+                    isEnd = true;
+                else if (value[0].indexX == 0 || value[0].indexX == 9 || value[0].indexY == 0 || value[0].indexY == 9)
+                    isEnd = true;
+                else if (value[0].isSysMarked)
+                    isEnd = true;
+                else {
+                    direction = value[1]
+                    xIndex = value[0].indexX;
+                    yIndex = value[0].indexY;
+                }
+            }
+        } 
+        for (var i = 1; i < 9; i++) {
+            var isEnd = false;
+            var xIndex = 8;
+            var yIndex = i;
+            var direction = moveDirection.TOP;
+            while (!isEnd) {
+                var value = moveNext(blackbox.board[xIndex][yIndex], direction);
+                if (value[0] == null)
+                    isEnd = true;
+                else if (value[0].indexX == 0 || value[0].indexX == 9 || value[0].indexY == 0 || value[0].indexY == 9)
+                    isEnd = true;
+                else if (value[0].isSysMarked)
+                    isEnd = true;
+                else {
+                    direction = value[1]
+                    xIndex = value[0].indexX;
+                    yIndex = value[0].indexY;
+                }
+            }
+        }        
+    };
+    blackbox.showLines = function () {
+
+    };
     blackbox.init();
     return blackbox;
 })();
--- GameZone/app/scripts/models/blackboxmodel.js
+++ GameZone/app/scripts/models/blackboxmodel.js
@@ -1,16 +1,94 @@
 ;
-module.exports = function (x, y) { 
+module.exports = function (x, y) {
+    var CellType = {
+        CORNER_BUTTON: "CORNER_BUTTON",
+        CORNER: "CORNER",
+        EDGE: "EDGE",
+        CELL: "CELL",
+    };
+    var LinePosition = {
+        TOP: 1,
+        BOTTOM: 2,
+        LEFT: 4,
+        RIGHT: 8
+    };
+    var LineType = {
+        BLANK: 0,
+        BOTTOM_LEFT: 6,
+        BOTTOM_LEFT_RIGHT: 14,
+        BOTTOM_RIGHT: 10,
+        CROSS: 15,
+        HORIZONTAL: 12,
+        TOP_BOTTOM_LEFT: 7,
+        TOP_BOTTOM_RIGHT: 11,
+        TOP_LEFT: 5,
+        TOP_LEFT_RIGHT: 13,
+        TOP_RIGHT: 9,
+        VERTICAL: 3,
+    };
+
     var obj = {
         indexX: x,
         indexY: y
     };
     var style = "";
-    if((x==0&&y==0) || (x==0&&y==9) || (x==9&&y==0) || (x==9&&y==9))
+    var cellType = CellType.CELL;
+    if ((x == 0 && y == 0) || (x == 0 && y == 9) || (x == 9 && y == 0) || (x == 9 && y == 9)) {
         style += "blackbox-cell-corner ";
-    else if(x == 0 || y == 0 || x == 9 || y == 9)
+        if (x == 0 && y == 0)
+            cellType = CellType.CORNER_BUTTON;
+        else
+            cellType = CellType.CORNER;
+    }
+    else if (x == 0 || y == 0 || x == 9 || y == 9) {
         style += "blackbox-cell-edge ";
-    else
-        style += "blackbox-cell ";
+        cellType = CellType.EDGE;
+    }
+    else {
+        style += "blackbox-cell blackbox-cell-bg-blank ";
+    }
     obj.style = style;
+    obj.cellType = cellType;
+    obj.isSysMarked = false;
+    obj.isUserMarked = false;
+    obj.edgeText = "";
+    obj.lineType = 0;
+    obj.click = function () {
+
+    };
+    obj.updateStyle = function () {
+        if (obj.cellType == CellType.CELL) {
+            var bgp = /blackbox-cell-bg-\w+/g;
+            if (obj.isSysMarked) {
+                obj.style = obj.style.replace(bgp, "blackbox-cell-bg-marked");
+            }
+            else {
+                if (obj.lineType == LineType.BLANK)
+                    obj.style = obj.style.replace(bgp, "blackbox-cell-bg-blank");
+                else if (obj.lineType == LineType.BOTTOM_LEFT)
+                    obj.style = obj.style.replace(bgp, "blackbox-cell-bg-bottomleft");
+                else if (obj.lineType == LineType.BOTTOM_LEFT_RIGHT)
+                    obj.style = obj.style.replace(bgp, "blackbox-cell-bg-bottomleftright");
+                else if (obj.lineType == LineType.BOTTOM_RIGHT)
+                    obj.style = obj.style.replace(bgp, "blackbox-cell-bg-bottomright");
+                else if (obj.lineType == LineType.CROSS)
+                    obj.style = obj.style.replace(bgp, "blackbox-cell-bg-cross");
+                else if (obj.lineType == LineType.HORIZONTAL)
+                    obj.style = obj.style.replace(bgp, "blackbox-cell-bg-horizontal");
+                else if (obj.lineType == LineType.TOP_BOTTOM_LEFT)
+                    obj.style = obj.style.replace(bgp, "blackbox-cell-bg-topbottomleft");
+                else if (obj.lineType == LineType.TOP_BOTTOM_RIGHT)
+                    obj.style = obj.style.replace(bgp, "blackbox-cell-bg-topbottomright");
+                else if (obj.lineType == LineType.TOP_LEFT)
+                    obj.style = obj.style.replace(bgp, "blackbox-cell-bg-topleft");
+                else if (obj.lineType == LineType.TOP_LEFT_RIGHT)
+                    obj.style = obj.style.replace(bgp, "blackbox-cell-bg-topleftright");
+                else if (obj.lineType == LineType.TOP_RIGHT)
+                    obj.style = obj.style.replace(bgp, "blackbox-cell-bg-topright");
+                else if (obj.lineType == LineType.VERTICAL)
+                    obj.style = obj.style.replace(bgp, "blackbox-cell-bg-vertical");
+            }
+        }
+    };
     return obj;
 };
